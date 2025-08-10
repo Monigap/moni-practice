3129e9f7670ecd565bf3b0b7bf405f80b255a8d1
Monigap <162157804+Monigap@users.noreply.github.com>
2025-08-10 23:06:58 +0530

    Create Class.java
    Write a program to accept the elements into an array and display the array from right to left

---
+++ Class.java
@@ -0,0 +1,25 @@
+Write a program to accept the elements into an array and display the array from right to left
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size;
+        size = sc.nextInt();
+        int[] a = new int[size];
+        for(int i=0;i<size;i++){
+            a[i] = sc.nextInt();
+        }
+        for(int i=size-1;i>=0;i--)
+        {
+            System.out.print(a[i]+" ");
+        }
+        }
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
