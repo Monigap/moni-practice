9117b5cd141e688f8ad06f5a7d56780573938b6b
Monigap <162157804+Monigap@users.noreply.github.com>
2025-08-10 23:16:49 +0530

    Create double.java
    Multiply two decimals and print the product with a precision of 2 decimal places .

---
+++ double.java
@@ -0,0 +1,18 @@
+Multiply two decimals and print the product with a precision of 2 decimal places .
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float a = sc.nextFloat();
+        float b = sc.nextFloat();
+        System.out.printf("%.2f",a*b);
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
