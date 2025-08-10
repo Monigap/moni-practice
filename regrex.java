9950d1a9782d31c55731c894c9205fe5c99ed673
Monigap <162157804+Monigap@users.noreply.github.com>
2025-08-10 23:17:45 +0530

    Create regrex.java
    Print the statement "Hello World" in printf without semicolon (;).

---
+++ regrex.java
@@ -0,0 +1,14 @@
+Print the statement "Hello World" in printf without semicolon (;).
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        if (System.out.printf("Hello World") != null) {}
+    }
+}
