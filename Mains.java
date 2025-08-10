716ca19a47ac1394286dab0c24c119359c59c0bc
Monigap <162157804+Monigap@users.noreply.github.com>
2025-08-10 23:11:54 +0530

    Create Mains.java
    Write a program to accept the element into an array and replace every element in the array with the sum of its every other element

---
+++ Mains.java
@@ -0,0 +1,25 @@
+Write a program to accept the element into an array and replace every element in the array with the sum of its every other element
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size,sum=0;
+        size = sc.nextInt();
+        int[] a = new int[size];
+        for(int i =0;i<size;i++){
+            a[i] = sc.nextInt();
+            sum = sum + a[i];
+        }
+        for(int i=0;i<size;i++){
+            System.out.print(sum-a[i]+ " ");
+        }
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
