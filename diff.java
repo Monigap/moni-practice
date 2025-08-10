eba835fcbd3f4f047972ec1aa27de5797e646eaa
Monigap <162157804+Monigap@users.noreply.github.com>
2025-08-10 23:14:03 +0530

    Create diff.java
    Get two numbers from the user and print the difference between the two numbers

---
+++ diff.java
@@ -0,0 +1,18 @@
+Get two numbers from the user and print the difference between the two numbers
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a-b);
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
