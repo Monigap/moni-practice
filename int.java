f50d9f880883d054387c5827f6d6349abe06f170
Monigap <162157804+Monigap@users.noreply.github.com>
2025-08-10 23:13:08 +0530

    Create int.java
    Get 2 integer inputs and display their sum

---
+++ int.java
@@ -0,0 +1,18 @@
+Get 2 integer inputs and display their sum
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        System.out.println("Sum of "+ n + " and "+ m +" is "+ (n+m));
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
