24f60202f5cdf5ac583f1ae1223fea5e746d50e3
Monigap <162157804+Monigap@users.noreply.github.com>
2025-08-10 23:22:05 +0530

    Create smallest.java
    Write a program to accept a array elements from the user and display the smallest element in the array.

---
+++ smallest.java
@@ -0,0 +1,27 @@
+Write a program to accept a array elements from the user and display the smallest element in the array.
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++){
+            a[i] = sc.nextInt();
+        }
+        int min = a[0];
+        for(int i = 1; i < n; i++){
+            if( a[i] < min){
+                min = a[i];
+            }
+        }
+
+        System.out.print(min + " ");
+    }
+}
