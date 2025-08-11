1f0c6013547d04f6b5ad8ef9d0a037c5aedaff07
Monigap <162157804+Monigap@users.noreply.github.com>
2025-08-11 22:29:28 +0530

    Create char.java
    Write a program to count the special characters in the given string

---
+++ char.java
@@ -0,0 +1,22 @@
+Write a program to count the special characters in the given string
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int u =0;
+        for(int i =0; i<s.length();i++){
+            char c = s.charAt(i);
+            if(!Character.isLetterOrDigit(c))
+                u++;
+        }
+        System.out.println(u);
+    }
+}
